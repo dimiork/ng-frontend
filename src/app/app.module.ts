581199cb7467287c9ebefd0f718a4cca20903afb
Kostya Shabalev <kvshabalev@gmail.com>
2018-12-06 16:32:41 +0200

    removed imports, added blank lines

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //Interceptors
 import { TokenInterceptor, ErrorInterceptor } from './interceptors';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
