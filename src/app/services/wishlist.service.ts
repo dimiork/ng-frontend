e284c98f7019ba38515598ea91271b3cb55f7e92
elena_grishina <elena_grishina@i.ua>
2018-12-07 11:45:27 +0200

    third fix)

--- src/app/services/wishlist.service.ts
+++ src/app/services/wishlist.service.ts
@@ -1,8 +1,7 @@
-import { Injectable, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
-import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
