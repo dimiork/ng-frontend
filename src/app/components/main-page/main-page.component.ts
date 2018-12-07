a9f60fc972827594594a1ac6b6cd114537dc50d4
dimiork <dimiork@gmail.com>
2018-12-07 18:13:57 +0200

    lint fix

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -10,23 +10,23 @@ import { Product } from '../../models/product.model';
 })
 export class MainPageComponent implements OnInit {
 
-  private products: Product[]; 
+  private products: Product[];
 
   constructor(private productsService: ProductsService) {
     this.products = [
       {
-        "id": "5b85097083d82c2de542be4b",
-        "title": "string",
-        "description": "string",
-        "category_id": "string",
-        "category_title": "string",
-        "price": 99.99,
-        "stock": 1,
-        "thumbnail": "string"
+        'id': '5b85097083d82c2de542be4b',
+        'title': 'string',
+        'description': 'string',
+        'category_id': 'string',
+        'category_title': 'string',
+        'price': 99.99,
+        'stock': 1,
+        'thumbnail': 'string'
       }];
   }
 
-  public ngOnInit() {
+  public ngOnInit(): void {
     this.productsService.getAllProducts().
       subscribe((products: Product[]) => {
         this.products = products;
