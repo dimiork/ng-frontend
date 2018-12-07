9775d9b4dd732e093c6e2b7db0f558f606298e9d
Kostya Shabalev <kvshabalev@gmail.com>
2018-12-07 12:08:34 +0200

    initial commit

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -10,7 +10,6 @@ import { AuthorizationService } from '../services/authorization.service';
 })
 export class AuthGuard implements CanActivate {
 
-<<<<<<< HEAD
   private hasAcces: boolean;
 
   constructor(
@@ -37,35 +36,6 @@ export class AuthGuard implements CanActivate {
     if (!this.hasAcces) {
       this.router.navigate(['login']);
     }
-=======
-	private hasAcces: boolean;
-
-	constructor(
-		private router: Router,
-		private authService: AuthService
-		) {
-
-		this.authService.getCurrentUser() // Check whether this function name matches the one in the authService
-		  .subscribe(client => {
-		  	if (!!client) {
-		  		this.router.navigate(['home']);
-		  		this.hasAcces = true;
-
-		  		return;
-		  	}
-
-		  	this.hasAcces = false;
-
-		  });
-	}
-
-  canActivate(): Observable<boolean> | boolean {
-
-  	if (!this.hasAcces) {
-  		this.router.navigate(['login']);
-  	}
-
->>>>>>> first commit v1.0
 
     return this.hasAcces;
   }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+	private loginUserData = {};
 
-  ngOnInit() {
-  }
+	constructor(
+		private authService: AuthService,
+		private router: Router
+		) {
+
+		if (localStorage.getItem('token')) {
+			this.router.navigate(['home']);
+		}
+	}
+
+	loginUser() {
+		if (Object.values(this.loginUserData).length) {
+			this.authService.route = 'login'; // Check var
+			this.authService.sendUserData(this.loginUserData) // Check meth
+			.subscribe(
+					result => {
+						this.router.navigate(['home']);
+					}
+				);
+		}
+	}
 
 }
