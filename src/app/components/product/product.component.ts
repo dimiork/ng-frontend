161dd45cd39a15ed973b17af104040a42beb1126
IgorOmelyuskho <omelyushko.igor@gmail.com>
2018-12-10 16:13:30 +0200

    remove unnecessary import

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { ProductsService } from '../../services/products.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
