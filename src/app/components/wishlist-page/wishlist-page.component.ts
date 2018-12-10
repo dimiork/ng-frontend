b4f8b582523a8314fd8ae4b051bc4e3339586b05
elena_grishina <elena_grishina@i.ua>
2018-12-10 13:53:29 +0200

    get wishlist by id

--- src/app/components/wishlist-page/wishlist-page.component.ts
+++ src/app/components/wishlist-page/wishlist-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../../services/wishlist.service';
 import { Product } from 'src/app/models/product.model';
+import { AuthorizationService } from 'src/app/services/authorization.service';
 
 @Component({
   selector: 'app-wishlist-page',
@@ -13,13 +14,23 @@ export class WishlistPageComponent implements OnInit {
 
   products: Product[];
 
-  constructor( private wishlistService: WishlistService ) {}
+  id: string;
+
+  constructor( private wishlistService: WishlistService,
+    private authorizationService: AuthorizationService ) {}
 
   ngOnInit(): void {
-    this.wishlistService.getWishlist()
-    .subscribe((data: any) => {
-      this.products = data.wishlists[0].items;
-      console.log(this.products);
+    this.authorizationService.getUser()
+    .subscribe(user => {
+      this.id = user.id;
+      console.log(this.id);
+      
+      this.wishlistService.getWishlistById(this.id)
+        .subscribe((data: any) => {
+          this.products = data.items;
+          console.log(data);
+      }, err => console.log(err));
     });
   }
+
 }
