05ce620ad43752bf550828fe7ac9a459e7c7a3b5
IgorOmelyuskho <36445648+IgorOmelyuskho@users.noreply.github.com>
2018-12-10 15:43:30 +0200

    Update app.module.ts
    remove productComponent from imports[]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,6 @@ import { LoginComponent } from './components/login/login.component';
     HttpClientModule,
     MaterialModule,
     AppRoutingModule,
-    productPage,
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
