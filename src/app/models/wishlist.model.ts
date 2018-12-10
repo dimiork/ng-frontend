8c4f9622c9ea534dca59660f4c89280ca6ebfd16
Elena Grishina <45165590+elena-grishina@users.noreply.github.com>
2018-12-10 09:02:29 +0200

    Delete wishlist.model.ts

--- src/app/models/wishlist.model.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '../model/user.model';
-import { Product } from '../model/product.model';
-
-export class Wishlist {
-    id: string;
-    client: User;
-    items: Product[];
-}
