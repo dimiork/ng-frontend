2e00a83b164a8a1038cd29a9002c9995ea06c953
IgorOmelyuskho <omelyushko.igor@gmail.com>
2018-12-11 14:26:39 +0200

    implement guard which prohibits the navigate to 'login' and 'register'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import { AuthGuard } from './guards/auth.guard';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DisableLoginGuard } from './guards/disable-login.guard';
 
 const routes: Routes = [
   { path: '', component: MainPageComponent, pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [DisableLoginGuard] },
+  { path: 'register', component: RegisterComponent, canActivate: [DisableLoginGuard] },
 ];
 
 @NgModule({
