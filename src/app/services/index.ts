b29de517d0a4f8fc4420cecfb194c0709927f6af
dimiork <dimiork@gmail.com>
2018-12-17 22:55:57 +0200

    fix tslint

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,2 +1,4 @@
 export { AuthorizationService } from './authorization.service';
 export { NotificationService } from './notification.service';
+export { ProductsService } from './products.service';
+export { WishlistService } from './wishlist.service';
