8dc1b29fc01d197436757e8aeece73217a193e89
Igor <omelyushko.igor@gmail.com>
2018-12-18 15:21:18 +0200

    fix bug in 'admin add product page'

--- src/app/components/admin-add-product/admin-add-product.component.ts
+++ src/app/components/admin-add-product/admin-add-product.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-import { Product } from '../../models/product.model';
-import { ProductsService } from '../../services/products.service';
+import { Product } from '../../models';
+import { ProductsService } from '../../services';
 
 @Component({
   selector: 'app-admin-add-product',
